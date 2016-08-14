c90501edb2d1787e901525753eb51a6845a06d59
Konstantin Gredeskoul <kig@goshippo.com>
2016-08-14 05:23:41 -0700

    Fixing Rspecs

--- spec/shhh/app/nlp_spec.rb
+++ spec/shhh/app/nlp_spec.rb
@@ -11,25 +11,30 @@ module Shhh
       # let(:result) { nlp.process }
 
       describe %Q(encrypt file 'hello' using 1234 save to output.enc) do
-        it { is_expected.to map_to(%w(--encrypt --file 'hello' --private_key 1234 --output output.enc))}
+        it { is_expected.to map_to(%w(--encrypt --file 'hello' --private_key 1234 --output output.enc)) }
       end
 
-      describe 'generate key to the clipboard and keychain item mykey' do
-        it { is_expected.to map_to(%w(--generate --copy --keychain mykey))}
-      end
-
-      describe %Q(edit file 'passwords.enc' ask about the key) do
-        it { is_expected.to map_to(%w(--edit --file 'passwords.enc' --interactive))}
+      describe %Q(edit file 'passwords.enc' ask the key) do
+        it { is_expected.to map_to(%w(--edit --file 'passwords.enc' --interactive)) }
       end
 
       describe 'decrypt file /etc/secrets with key from $file save to ./secrets' do
-        it { is_expected.to map_to(%w(--decrypt --file /etc/secrets --keyfile $file --output ./secrets))}
+        it { is_expected.to map_to(%w(--decrypt --file /etc/secrets --keyfile $file --output ./secrets)) }
       end
 
-      describe 'lock file $input with keychain $name' do
-        it { is_expected.to map_to(%w(--encrypt --file $input --keychain $name))}
-      end
+      if Shhh::App.is_osx?
 
+        context 'when using the keychain' do
+          describe 'generate key to the clipboard and keychain item mykey' do
+            it { is_expected.to map_to(%w(--generate --copy --keychain mykey)) }
+          end
+
+          describe 'lock file $input with keychain $name' do
+            it { is_expected.to map_to(%w(--encrypt --file $input --keychain $name)) }
+          end
+        end
+
+      end
     end
   end
 end
