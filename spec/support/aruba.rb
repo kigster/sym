14e03ea078407ed475562a40961481b842b186dd
Konstantin Gredeskoul <kigster@gmail.com>
2020-08-14 07:58:32 -0700

    Do not run aruba in process

--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -3,8 +3,6 @@ require 'aruba/processes/in_process'
 require 'sym/app/cli'
 
 Aruba.configure do |config|
-  unless ENV['CI']
-    config.command_launcher = :in_process
-    config.main_class = Sym::App::CLI
-  end
+  config.command_launcher = :in_process
+  config.main_class       = Sym::App::CLI
 end
